65fbc22add7ea6b6497bd54e5cefdfe905bd8791
Lilko Petkov <lilko.petkov@Lilkos-MacBook-Air.local>
2024-03-24 13:26:57 +0200

    Adjusted directory convert logic

--- functions/funcs.py
+++ functions/funcs.py
@@ -16,8 +16,8 @@ def convert(file: str, extension: str, output_file: str, width: int = 0, height:
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
     if result.returncode == 0:
-        print(f"{bg.green}Image {file} successfully converted to {extension}{c.reset}")
-        print(f"{bg.green}New file: {output_file}.{extension}{c.reset}")
+        print(f"{bg.green}Success: Image {file} successfully converted to {extension}{c.reset}")
+        print(f"{bg.green}Success: New file: {output_file}.{extension}{c.reset}")
     else:
         print(f"{c.bold}{fg.red}{result.stderr}{c.reset}")
 
@@ -31,23 +31,23 @@ def convert_all(path: str, extension: str, exceptions: List[str], all: bool = Fa
 
     if log:
         result = subprocess.run("find . -name image_convert.log -exec realpath {} \\;", shell=True, capture_output=True, text=True)
-        print(f"{bg.green}Image convert log/s: {result.stdout}{c.reset}")
+        print(f"{bg.green}Success: Image convert log created{c.reset}")
 
     for file in os.listdir(path):
-        if os.path.isfile(file) and file not in exceptions:
+        if os.path.isfile(os.path.join(path, file)) and file not in exceptions:
     
-            command = f"magick {file} {file}.{extension}" if width == 0 and height == 0 else\
-            f"magick {file} -resize {width}x{height} {file}.{extension}"
+            command = f"magick {os.path.join(path, file)} {os.path.join(path, file)}.{extension}" if width == 0 and height == 0 else\
+            f"magick {os.path.join(path, file)} -resize {width}x{height} {os.path.join(path, file)}.{extension}"
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
             if result.returncode == 0:
                 if log:
                     with open("image_convert.log", "a") as f:
-                        f.write(f"[{str(datetime.now(timezone.utc))}] {file} {file}.{extension}\n")
+                        f.write(f"[{str(datetime.now(timezone.utc))}] {os.path.join(path, file)} {os.path.join(path, file)}.{extension}\n")
             else:
                 if log:
                     with open("image_convert.log", "a") as f:
-                        f.write(f"[{str(datetime.now(timezone.utc))}] {file} {file}.{extension} [{result.stderr}]")
+                        f.write(f"[{str(datetime.now(timezone.utc))}] {os.path.join(path, file)} {os.path.join(path, file)}.{extension} [{result.stderr}]")
 
 
 # Delete all created files from log
@@ -58,4 +58,4 @@ def delete(pathToLog: Union[None, str]) -> None:
     if result.stderr:
         print(f"{c.bold}{fg.red}Error: image_convert.log might be missing, use -h for help{c.reset}")
     else:
-        print(f"{bg.green}Converted images removed{c.reset}")
+        print(f"{bg.green}Success: Converted images removed{c.reset}")
