21e03d93bccbfd3c3fcc76d8302e6908dad238c9
JaschaHaifetz <lilko.petkovv@gmail.com>
2024-04-22 13:50:48 +0300

    Adding logging email option

--- log/log.py
+++ log/log.py
@@ -1,12 +1,11 @@
 import logging 
 import time
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-from pathlib import Path
-
-# Logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 # Handlers
 file_handler = logging.FileHandler(f"{os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs', 'app.log')}", mode="a")
@@ -21,5 +20,43 @@ formatter = logging.Formatter(
 
 file_handler.setFormatter(formatter)
 
-# Add handlers to the logger
+# Configure email handler
+class EmailNotificationHandler(logging.Handler):
+    def __init__(self, mail_host, mail_port, sender_email, sender_password, receiver_email):
+        super().__init__()
+        self.mail_host = mail_host
+        self.mail_port = mail_port
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+        self.receiver_email = receiver_email
+
+    def emit(self, record):
+        subject = f"Error Log Notification - {record.levelname}"
+        body = self.format(record)
+        message = MIMEMultipart()
+        message["From"] = self.sender_email
+        message["To"] = self.receiver_email
+        message["Subject"] = subject
+        message.attach(MIMEText(body, "plain"))
+
+        with smtplib.SMTP_SSL(self.mail_host, self.mail_port) as server:
+            server.login(self.sender_email, self.sender_password)
+            server.sendmail(self.sender_email, self.receiver_email, message.as_string())
+
+
+# Add email handler to the logger (change the SMTP settings accordingly)
+email_handler = EmailNotificationHandler(
+    mail_host="MAILSERVER",
+    mail_port=465,
+    sender_email="SENDER",
+    sender_password="PASSWORD",
+    receiver_email="RECIPIENT"
+)
+
+email_handler.setLevel(logging.WARNING)  # Send email notifications only for WARNING level and above
+logger.addHandler(email_handler)
 logger.addHandler(file_handler)
+
+# Example usage
+logger.debug("This is a debug message")
+logger.warning("This is a warning message")
